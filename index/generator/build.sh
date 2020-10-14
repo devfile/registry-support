6613e050597b8a808d72389885378f8c3e3f1918
John Collier <jcollier@redhat.com>
2020-10-14 14:10:09 -0400

    Enable GitHub Actions for CI
    Signed-off-by: John Collier <jcollier@redhat.com>

--- index/generator/build.sh
+++ index/generator/build.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-MODULE="github.com/devfile/registry-support/index/generator"
-BIN_DIR=$GOPATH/bin
-MODULE_BIN=$BIN_DIR/generator
-
-CGO_ENABLED=0 go install $MODULE
-cp $MODULE_BIN ./index-generator
+CGO_ENABLED=0 go build -mod=vendor -o index-generator .
