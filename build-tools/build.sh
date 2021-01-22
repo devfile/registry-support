d3198547514e6a4b5b5ea2aeeb6f90761cba92c7
John Collier <jcollier@redhat.com>
2021-01-22 11:33:58 -0500

    Support archive.tar on the registry build script (#21)
    * Support archive.tar on the registry build script
    
    Signed-off-by: John Collier <jcollier@redhat.com>
    
    * Remove unneeded file from exclusion
    
    Signed-off-by: John Collier <jcollier@redhat.com>

--- build-tools/build.sh
+++ build-tools/build.sh
@@ -7,7 +7,10 @@
 # SPDX-License-Identifier: EPL-2.0
 #
 
-#!/bin/sh
+#!/bin/bash
+
+# Enable extended globbing in the shell
+shopt -s extglob
 
 buildToolsFolder="$(dirname "$0")"
 generatorFolder=$buildToolsFolder/../index/generator
@@ -17,12 +20,20 @@ display_usage() {
   echo "usage: build.sh <path-to-registry-repository-folder>" 
 } 
 
+# cleanup_and_exit removes the temp folder we created and exits with the exit code passed into it
+clenaup_and_exit() {
+  rm -rf $registryFolder
+  exit $1
+}
 # Check if a registry repository folder was passed in, if not, exit
-registryFolder=$1
 if [ $# -ne 1 ]; then
   display_usage
   exit 1
 fi
+
+# Copy the registry repository over to a temp folder
+registryFolder=$(mktemp -d)
+cp -rf $1/. $registryFolder/
 
 cd $generatorFolder
 
@@ -31,7 +42,7 @@ echo "Building index-generator tool"
 ./build.sh
 if [ $? -ne 0 ]; then
   echo "Failed to build index-generator tool"
-  exit 1
+  clenaup_and_exit 1
 fi
 echo -e "Successfully built the index-generator tool\n"
 
@@ -42,16 +53,37 @@ echo "Generate the devfile registry index"
 $generatorFolder/index-generator $registryFolder/stacks $registryFolder/index.json
 if [ $? -ne 0 ]; then
   echo "Failed to build the devfile registry index"
-  exit 1
+  clenaup_and_exit 1
 fi
 echo -e "Successfully built the devfile registry index\n"
+
+# Generate the tar archive
+for stackDir in $registryFolder/stacks/*/
+do
+  cd $stackDir
+  # Find the files to add to the tar archive
+  tarFiles=$(find . \( -not -name 'devfile.yaml' \
+    -a -not -name "meta.yaml" \
+    -a -not -name "*.vsx" \
+    -a -not -name "." \
+    -a -not -name "logo.svg" \
+    -a -not -name "logo.png" \) -maxdepth 1)
+
+  # There are files that need to be pulled into a tar archive
+  if [[ ! -z $tarFiles ]]; then
+    tar -cvf archive.tar $tarFiles
+    rm -rf $tarFiles
+  fi
+  cd "$OLDPWD"
+done
 
 # Build the Docker image containing the devfile stacks and index.json
 echo "Building the devfile registry index container"
 docker build -t devfile-index -f $buildToolsFolder/Dockerfile $registryFolder
 if [ $? -ne 0 ]; then
   echo "Failed to build the devfile registry index container"
-  exit 1
+  clenaup_and_exit 1
 fi
 
 echo "Successfully built the devfile registry index container"
+clenaup_and_exit
