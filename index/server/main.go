063cdd48cfd7c42dca0035ec8db290acdfa27ff9
Jingfu Wang <jingfwan@redhat.com>
2021-06-03 15:20:16 -0400

    feat: base64 encoding for icon
    Signed-off-by: Jingfu Wang <jingfwan@redhat.com>

--- index/server/main.go
+++ index/server/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path"
 	"path/filepath"
@@ -149,12 +151,29 @@ func main() {
 
 	router.GET("/index/:type", func(c *gin.Context) {
 		indexType := c.Param("type")
+		iconType := c.Query("icon")
 
 		// Serve the index with type
 		if indexType == string(indexSchema.SampleDevfileType) {
-			c.File(sampleIndexPath)
+			if iconType == "base64" {
+				bytes, err := encodeIndexIconToBase64(sampleIndexPath)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				c.Data(http.StatusOK, http.DetectContentType(bytes), bytes)
+			} else {
+				c.File(sampleIndexPath)
+			}
 		} else if indexType == "all" {
-			c.File(indexPath)
+			if iconType == "base64" {
+				bytes, err := encodeIndexIconToBase64(indexPath)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				c.Data(http.StatusOK, http.DetectContentType(bytes), bytes)
+			} else {
+				c.File(indexPath)
+			}
 		} else {
 			c.JSON(http.StatusNotFound, gin.H{
 				"status": fmt.Sprintf("the devfile with %s type didn't exist", indexType),
@@ -300,5 +319,86 @@ func serveDevfileIndex(c *gin.Context) {
 	}()
 
 	// Serve the index.json file
-	c.File(stackIndexPath)
+	iconType := c.Query("icon")
+	if iconType == "base64" {
+		bytes, err := encodeIndexIconToBase64(stackIndexPath)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		c.Data(http.StatusOK, http.DetectContentType(bytes), bytes)
+	} else {
+		c.File(stackIndexPath)
+	}
+}
+
+// encodeIndexIconToBase64 encodes all index icons to base64 format given the index file path
+func encodeIndexIconToBase64(indexPath string) ([]byte, error) {
+	// load index
+	bytes, err := ioutil.ReadFile(indexPath)
+	if err != nil {
+		return nil, err
+	}
+	var index []indexSchema.Schema
+	err = json.Unmarshal(bytes, &index)
+	if err != nil {
+		return nil, err
+	}
+
+	// encode all index icons to base64 format
+	for i, indexEntry := range index {
+		if indexEntry.Icon != "" {
+			base64Icon, err := encodeToBase64(indexEntry.Icon)
+			index[i].Icon = base64Icon
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	bytes, err = json.MarshalIndent(&index, "", "  ")
+	if err != nil {
+		return nil, err
+	}
+	return bytes, nil
+}
+
+// encodeToBase64 encodes the content from the given uri to base64 format
+func encodeToBase64(uri string) (string, error) {
+	url, err := url.Parse(uri)
+	if err != nil {
+		return "", err
+	}
+
+	// load the content from the given uri
+	var bytes []byte
+	if url.Scheme == "http" || url.Scheme == "https" {
+		resp, err := http.Get(uri)
+		if err != nil {
+			return "", err
+		}
+		defer resp.Body.Close()
+
+		bytes, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+	} else {
+		bytes, err = ioutil.ReadFile(uri)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	// encode the content to base64 format
+	var base64Encoding string
+	mimeType := http.DetectContentType(bytes)
+	switch mimeType {
+	case "image/jpeg":
+		base64Encoding += "data:image/jpeg;base64,"
+	case "image/png":
+		base64Encoding += "data:image/png;base64,"
+	default:
+		base64Encoding += "data:image/svg+xml;base64,"
+	}
+	base64Encoding += base64.StdEncoding.EncodeToString(bytes)
+	return base64Encoding, nil
 }
