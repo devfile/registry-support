1afc80fd396006e703f76faf4f7920ae8290ba2e
Jingfu Wang <jingfwan@redhat.com>
2021-07-05 16:56:56 -0400

    feat: self-signed certificates support
    Signed-off-by: Jingfu Wang <jingfwan@redhat.com>

--- registry-library/cmd/root.go
+++ registry-library/cmd/root.go
@@ -35,6 +35,7 @@ var (
 	allResources bool
 	destDir      string
 	devfileType  string
+	insecure     bool
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -80,9 +81,9 @@ func init() {
 			var err error
 
 			if allResources {
-				err = library.PullStackFromRegistry(registry, stack, destDir)
+				err = library.PullStackFromRegistry(registry, stack, destDir, insecure)
 			} else {
-				err = library.PullStackByMediaTypesFromRegistry(registry, stack, library.DevfileMediaTypeList, destDir)
+				err = library.PullStackByMediaTypesFromRegistry(registry, stack, library.DevfileMediaTypeList, destDir, insecure)
 			}
 			if err != nil {
 				fmt.Printf("Failed to pull %s from registry %s: %v\n", stack, registry, err)
@@ -91,6 +92,7 @@ func init() {
 	}
 	pullCmd.Flags().BoolVarP(&allResources, "all", "a", false, "pull all resources of the given stack")
 	pullCmd.Flags().StringVar(&destDir, "context", ".", "destination directory that stores stack resources")
+	pullCmd.Flags().BoolVar(&insecure, "insecure", false, "skip verification for security")
 
 	var listCmd = &cobra.Command{
 		Use:   "list",
@@ -100,13 +102,14 @@ func init() {
 				fmt.Printf("Please specify the devfile type by using flag --type\n")
 				return
 			}
-			err := library.PrintRegistry(registryList, devfileType)
+			err := library.PrintRegistry(registryList, devfileType, insecure)
 			if err != nil {
 				fmt.Printf("Failed to list stacks of registry %s: %v\n", registryList, err)
 			}
 		},
 	}
 	listCmd.Flags().StringVar(&devfileType, "type", "", "specify devfile type")
+	listCmd.Flags().BoolVar(&insecure, "insecure", false, "skip verification for security")
 
 	rootCmd.AddCommand(pullCmd, listCmd)
 }
