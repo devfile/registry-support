8e506205fd5f5811f9033297ab9afd69ecf006fe
Kim Tsao <ktsao@redhat.com>
2022-02-11 16:47:34 -0500

    change to bash script and expand aliases
    Signed-off-by: Kim Tsao <ktsao@redhat.com>

--- build_registry.sh
+++ build_registry.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script builds a devfile registry index container image based on the mock devfile registry data under tests/registry
 # This can be useful if developing components within this repository (such as the index server or build tools)
 # and want to test all of the components together
-
+shopt -s expand_aliases
 set -eux
 #set the docker alias if necessary
 . ./setenv.sh
