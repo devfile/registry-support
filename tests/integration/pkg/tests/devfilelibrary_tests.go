88ac426032c1eeeee3a4af00ba6709169f89cfea
Michael Valdron <mvaldron@redhat.com>
2022-07-08 17:11:01 -0400

    registry library download starter project integration test case added.
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- tests/integration/pkg/tests/devfilelibrary_tests.go
+++ tests/integration/pkg/tests/devfilelibrary_tests.go
@@ -30,12 +30,13 @@ import (
 )
 
 const (
-	nodejsStack    = "nodejs"
-	javaMavenStack = "java-maven"
-	quarkusStack   = "java-quarkus"
-	nodejsSample   = "nodejs-basic"
-	quarkusSample  = "code-with-quarkus"
-	pythonSample   = "python-basic"
+	nodejsStack      = "nodejs"
+	javaMavenStack   = "java-maven"
+	quarkusStack     = "java-quarkus"
+	nodejsSample     = "nodejs-basic"
+	quarkusSample    = "code-with-quarkus"
+	pythonSample     = "python-basic"
+	javaMavenStarter = "springbootproject"
 )
 
 var (
@@ -123,5 +124,14 @@ var _ = ginkgo.Describe("[Verify registry library works with registry]", func() 
 		}
 		_, _, err = devfilePkg.ParseDevfileAndValidate(parserArgs)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	})
+
+	ginkgo.It("should properly download stack starter project", func() {
+		tempDir := os.TempDir()
+		util.CmdShouldPass("registry-library", "download", publicDevfileRegistry, javaMavenStack, javaMavenStarter, "--context", tempDir)
+		starterPath := path.Join(tempDir, javaMavenStarter)
+		info, err := os.Stat(starterPath)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		gomega.Expect(info.IsDir()).To(gomega.Equal(true))
 	})
 })
