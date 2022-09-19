2347f9c316b47bc183506f5957238fd17c9cc784
Kim Tsao <ktsao@redhat.com>
2022-09-19 18:20:24 -0400

    license files
    Signed-off-by: Kim Tsao <ktsao@redhat.com>

---
+++ check_licenses.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script runs license checks on go files to determine if they are missing
+
+if ! command -v addlicense 2> /dev/null
+then
+  echo "error addlicense must be installed for this rule: go get -u github.com/google/addlicense" && exit 1
+elif ! addlicense -check -f license_header.txt $(find . -not -path '*/\.*' -not -path '*/vendor/*' -name '*.go'); then
+    echo "Licenses are not formatted; run './add_licenses.sh'"; exit 1 ;
+fi
+
