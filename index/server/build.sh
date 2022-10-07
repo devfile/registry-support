e6e2df91e8fe4350d2313a798ceb7b67a0e27afc
Michael Valdron <mvaldron@redhat.com>
2022-10-07 01:50:11 -0400

    registry-viewer repo cloning replaced with devfile-web repo cloning, registry-viewer docker image building removed.
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- index/server/build.sh
+++ index/server/build.sh
@@ -3,14 +3,11 @@
 # Build the index container for the registry
 buildfolder="$(realpath $(dirname ${BASH_SOURCE[0]}))"
 
-# Clone the registry-support repo
-if [ -d $buildfolder/registry-viewer ]; then
-	rm -rf $buildfolder/registry-viewer
+# Clone the devfile-web repo
+if [ -d $buildfolder/devfile-web ]; then
+	rm -rf $buildfolder/devfile-web
 fi
-git clone https://github.com/devfile/registry-viewer.git $buildfolder/registry-viewer
-
-# Build the registry viewer
-docker build -t registry-viewer --target builder --build-arg DEVFILE_VIEWER_ROOT=/viewer --build-arg DEVFILE_COMMUNITY_HOST=false $buildfolder/registry-viewer
+git clone https://github.com/devfile/devfile-web.git $buildfolder/devfile-web
 
 # Build the index server
 docker build -t devfile-index-base:latest $buildfolder
