d17f9129fe19bd1f3428fe1c457346dbcf6d1096
Michael Valdron <mvaldron@redhat.com>
2022-10-14 16:47:32 -0400

    Registry Viewer build & export removed from devfile-index-base image build
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- index/server/build.sh
+++ index/server/build.sh
@@ -3,11 +3,5 @@
 # Build the index container for the registry
 buildfolder="$(realpath $(dirname ${BASH_SOURCE[0]}))"
 
-# Clone the devfile-web repo
-if [ -d $buildfolder/devfile-web ]; then
-	rm -rf $buildfolder/devfile-web
-fi
-git clone https://github.com/devfile/devfile-web.git $buildfolder/devfile-web
-
 # Build the index server
 docker build -t devfile-index-base:latest $buildfolder
