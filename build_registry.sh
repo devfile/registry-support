79de72193b10af6bb626400b4533016b02bb5813
Michael Valdron <mvaldron@redhat.com>
2022-10-17 17:28:12 -0400

    build_registry.sh script now includes build script arguments 'headless' & 'static_files_path'.
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- build_registry.sh
+++ build_registry.sh
@@ -8,8 +8,28 @@ set -eux
 #set the docker alias if necessary
 . ./setenv.sh
 
+# Check if headless arg is passed
+if [ -z "${1+x}" ]
+then
+    headless=true
+else
+    headless=$1
+fi
+
+# Check if static-files-path is passed
+if [ -z "${2+x}" ]
+then
+    static_files_path=
+else
+    static_files_path=$2
+fi
+
 # Build the index server base image
 . ./index/server/build.sh
 
 # Build the test devfile registry image
+<<<<<<< HEAD
 docker build -t devfile-index:latest -f .ci/Dockerfile .
+=======
+docker build -t devfile-index:latest --build-arg headless=$headless --build-arg static_files_path=$static_files_path -f .ci/Dockerfile .
+>>>>>>> f586a44 (build_registry.sh script now includes build script arguments 'headless' & 'static_files_path'.)
