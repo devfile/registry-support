94537ee821fd5a04927774507c6dd763460834f0
Michael Valdron <mvaldron@redhat.com>
2023-06-30 16:28:50 -0400

    codegen happens on build
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- index/server/build.sh
+++ index/server/build.sh
@@ -3,5 +3,8 @@
 # Build the index container for the registry
 buildfolder="$(realpath $(dirname ${BASH_SOURCE[0]}))"
 
+# Generate OpenAPI endpoint and type definitions
+bash ${buildfolder}/codegen.sh
+
 # Build the index server
 docker build -t devfile-index-base:latest $buildfolder
--- index/server/codegen.sh
+++ index/server/codegen.sh
@@ -5,10 +5,20 @@ projectfolder="$(realpath $(dirname ${BASH_SOURCE[0]}))"
 
 # Generate source types from OpenAPI spec
 echo "Generating type source.."
-cd ${projectfolder} && ${GOPATH}/bin/oapi-codegen -config types.cfg.yaml openapi.yaml && cd - > /dev/null
+cd ${projectfolder} && ${GOPATH}/bin/oapi-codegen -config config/types.yaml openapi.yaml && cd - > /dev/null
+if [ $? != 0 ]
+then
+    echo "error with generating type source "
+    exit 1
+fi
 
 # Generate endpoint bindings source from OpenAPI spec
 echo "Generating endpoint bindings source.."
-cd ${projectfolder} && ${GOPATH}/bin/oapi-codegen -config endpoint.cfg.yaml openapi.yaml && cd - > /dev/null
+cd ${projectfolder} && ${GOPATH}/bin/oapi-codegen -config config/endpoint.yaml openapi.yaml && cd - > /dev/null
+if [ $? != 0 ]
+then
+    echo "error with generating endpoint bindings source "
+    exit 1
+fi
 
 echo "Done."
