7127893c9983b5341bc0c21d861f66176520f568
thepetk <thepetk@gmail.com>
2023-12-05 15:45:48 +0000

    Finalize test cases fixes
    Signed-off-by: thepetk <thepetk@gmail.com>

--- index/generator/library/library.go
+++ index/generator/library/library.go
@@ -295,12 +295,11 @@ func parseDevfileRegistry(registryDirPath string, force bool) ([]schema.Schema, 
 				}
 			}
 		} else { // if stack.yaml not exist, old stack repo struct, directly lookfor & parse devfile.yaml
-			versionComponent := schema.Version{}
+			versionComponent := schema.Version{Default: true}
 			err := parseStackDevfile(stackFolderPath, stackFolderDir.Name(), force, &versionComponent, &indexComponent)
 			if err != nil {
 				return nil, err
 			}
-			versionComponent.Default = true
 			indexComponent.Versions = append(indexComponent.Versions, versionComponent)
 		}
 		indexComponent.Type = schema.StackDevfileType
